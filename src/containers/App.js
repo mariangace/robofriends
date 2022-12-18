7940be46471576fd2e62034064814054beacca16
Mariana García Cervantes <mariangace@gmail.com>
2022-12-18 17:17:19 +0100

    update app class component to be a functional component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,50 +1,47 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
-import { robots } from "../robots";
 import "./App.css";
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: robots,
-      searchfield: "",
-    };
-  }
 
-  componentDidMount() {
-    // this is part of react so we don't user our => function because it's react
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  onSearchChange = (event) => {
-    //we use the arrows to force a bind of this. Otherwise this referes to the input, where we are calling this change
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filterdRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filterdRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
-  }
+  const filterdRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
+  const getRobots = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  };
+
+  useEffect(() => {
+    getRobots();
+  }, []);
+
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1>RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filterdRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
